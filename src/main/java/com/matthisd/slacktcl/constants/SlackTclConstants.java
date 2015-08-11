d5a535445145cdbe9326ba585507506b01dabc8b
Matthis Duclos <matthis.duclos@gmail.com>
2015-08-11 21:50:33 +0200

    Fix #3

--- src/main/java/com/matthisd/slacktcl/constants/SlackTclConstants.java
+++ src/main/java/com/matthisd/slacktcl/constants/SlackTclConstants.java
@@ -2,13 +2,13 @@ package com.matthisd.slacktcl.constants;
 
 public class SlackTclConstants {
 
-    public static final String FULL_STATION_PATTERN = "^([\\w\\s\\.-\\\\']+):([\\w\\s]+):([\\w\\s\\.-\\\\']+)$";
+    public static final String FULL_STATION_PATTERN = "^([a-zA-Z0-9'\\s\\.-]+):([\\w\\s]+):([a-zA-Z0-9'\\s\\.-]+)$";
 
-    public static final String STATION_BUS_PATTERN = "^([\\w\\s\\.-\\\\']+):([\\w\\s]+)$";
+    public static final String STATION_BUS_PATTERN = "^([a-zA-Z0-9'\\s\\.-]+):([\\w\\s]+)$";
 
-    public static final String STATION_PATTERN = "^([\\w\\s\\.-\\\\']+)$";
+    public static final String STATION_PATTERN = "^([a-zA-Z0-9'\\s\\.-]+)$";
 
-    public static final String LIST_PATTERN = "^list\\s([\\w\\s\\.-\\\\']+)$";
+    public static final String LIST_PATTERN = "^list\\s([a-zA-Z0-9'\\s\\.-]+)$";
 
     public static final String JSON_STATION_ID = "id";
 
